54db000dc42b42c31d92e922fe9022394a6cdb74
Ian <ian.vgs@hotmail.com>
2023-05-16 09:51:52 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   await app.startAllMicroservices(); */
 
   const configService = app.get(ConfigService);
-  app.enableCors(/* { origin: '*' } */)
+  app.enableCors({ origin: '*' })
   // da pra colocar o PORT no dotenv
   /* app.enableCors({
     origin: '*',
