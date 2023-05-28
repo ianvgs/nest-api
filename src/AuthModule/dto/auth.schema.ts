311474fed795e5ba703f39ae6000f594fe3c132d
Ian <ian.vgs@hotmail.com>
2023-05-28 01:08:40 -0300

    .

--- src/AuthModule/dto/auth.schema.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from 'mongoose';
-
-//Schema/Model no mongoDB com a estrutura da collection que vai usar
-export const AuthSchema = new mongoose.Schema({
-  auth: { type: String },
-  sessionKey: String,
-  //Sessão que expira, mongo limpa depois de certo tempo
-  createdAT: { type: Date, expires: 100, default: Date.now },
-});
--- src/AuthModule/dto/auth.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from 'mongoose';
-
-//MongoDb Pluraliza para colletion: "auths"
-export class Auth extends Document {
-  auth: string;
-}
